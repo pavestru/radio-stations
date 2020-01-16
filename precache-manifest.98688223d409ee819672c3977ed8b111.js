db615caa3843f3283ba128892254d837840b0a3a
Pavel Struhar <pstruhar@gmail.com>
2020-01-16 17:44:13 +0100

    Updates

---
+++ precache-manifest.98688223d409ee819672c3977ed8b111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda41c93121e95a193404bb5a9bea254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b2b019856318a5b043",
+    "url": "/static/css/main.9c499735.chunk.css"
+  },
+  {
+    "revision": "8cdcd1e1b13fbb394070",
+    "url": "/static/js/2.a1db6dee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a1db6dee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40b2b019856318a5b043",
+    "url": "/static/js/main.f7147a2f.chunk.js"
+  },
+  {
+    "revision": "8485dccc79c8c3c473f2",
+    "url": "/static/js/runtime-main.3c8b8b56.js"
+  }
+]);
